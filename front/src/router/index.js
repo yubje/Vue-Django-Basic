8192c63e8c84e8a015db0bfff4bf5bacfcdc4378
제유빈 <yubinje630@gmail.com>
2020-06-09 21:40:01 +0900

    Frontend | 01 added components - accounts, articles

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import LoginView from '../views/accounts/LoginView.vue'
+import SignupView from '../views/accounts/SignupView.vue'
+import ListView from '../views/articles/ListView.vue'
+import CreateView from '../views/articles/CreateView.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +14,26 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/accounts/login',
+    name: 'LoginView',
+    component: LoginView,
+  },
+  {
+    path: '/accounts/signup',
+    name: 'SignupView',
+    component: SignupView,
+  },
+  {
+    path: '/articles',
+    name: 'ListView',
+    component: ListView,
+  },
+  {
+    path: '/articles/create',
+    name: 'CreateView',
+    component: CreateView,
   },
 ]
 
---
+++ front/src/views/articles/CreateView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateView',
+}
+</script>
+
+<style>
+
+</style>
