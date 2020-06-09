add5408804d2cbdcf619f1c5d63defd2ce32d671
제유빈 <yubinje630@gmail.com>
2020-06-09 23:38:41 +0900

    Frontend | 04 Logout

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link v-if="!isLoggedin" :to="{ name: 'LoginView' }">Login</router-link>
+      <router-link to="/">Home</router-link> | 
+      <router-link v-if="!isLoggedin" :to="{ name: 'LoginView' }">Login</router-link> | 
+      <router-link v-if="isLoggedin" to="/accounts/logout" @click.native="logout">Logout</router-link>
 
     </div>
     <router-view
@@ -25,7 +26,9 @@ export default {
   },
   methods: {
     setCookie(key) {
+      // 3. 쿠키 설정
       this.$cookies.set('auth-token', key)
+      // 4. 로그인 완료 표시 
       this.isLoggedin = true
     },
     login(loginData) {
@@ -38,6 +41,25 @@ export default {
           this.$router.push('/')
         })
         .catch(error => console.log(error.response))
+    },
+    logout() {
+      // 1. 로그아웃 하기 위한 header 정보 만들기 
+      const requestHeader = {
+        headers: {
+          // 2. get을 통해 'auth-token'이라는 이름으로 저장된 토큰 값 가져오기
+          Authorization: `Token ${this.$cookies.get('auth-token')}`
+        }
+      }
+      // 3. post 요청은 두 번째 위치 인자로 data가 들어가기 때문에 비워둔다. 
+      axios.post(`${SERVER_URL}/rest-auth/logout/`, null, requestHeader)
+        .then(() => {
+          // 4. 브라우저 쿠키 값 지우기 
+          this.$cookies.remove('auth-token')
+          // 5. 로그인 여부 상태 변경 & Home으로 라우팅 
+          this.isLoggedin = false
+          this.$router.push('/')
+        })
+        .catch(error => console.log(error.response.data))
     },
   },
 }
