193bdf769f2d35c928fc500765e0eb36ef12f6b4
제유빈 <yubinje630@gmail.com>
2020-06-09 20:13:10 +0900

    Backend | 07 articles - create new article

--- back/articles/views.py
+++ back/articles/views.py
@@ -4,10 +4,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Article
-from .serializers import ArticleListSerializer
+from .serializers import ArticleListSerializer, ArticleSerializer
 
 @api_view(['GET'])
 def index(request):
   articles = Article.objects.order_by('-pk')
   serializer = ArticleListSerializer(articles, many=True)
   return Response(serializer.data)
+
+@api_view(['POST'])
+def create(request):
+  serializer = ArticleSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data)
+
+
+
