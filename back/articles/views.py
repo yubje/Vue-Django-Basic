b8e7735ccf93102dc46f4eea92494d7db248591f
제유빈 <yubinje630@gmail.com>
2020-06-09 20:09:35 +0900

    Backend | 06 articles - index

--- back/articles/views.py
+++ back/articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
+
+from .models import Article
+from .serializers import ArticleListSerializer
+
+@api_view(['GET'])
+def index(request):
+  articles = Article.objects.order_by('-pk')
+  serializer = ArticleListSerializer(articles, many=True)
+  return Response(serializer.data)
